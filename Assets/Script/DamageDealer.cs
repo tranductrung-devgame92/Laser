498d32305ff9968e81a9605e86e5ff8dd69c908e
brewman-tech-pixel <78021461+brewman-tech-pixel@users.noreply.github.com>
2021-01-28 11:41:29 +0700

    Initial commit

---
+++ Assets/Script/DamageDealer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    //
+    [SerializeField] int damage = 100;
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public int GetDamage()
+    {
+        return damage;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void Hit()
+    {
+        Destroy(gameObject);
+    }
+}
+   
+
---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //Khởi tạo giá trị health
+    [SerializeField] float health = 100;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject projectile;
+    [SerializeField] float projectileSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountDownAndShoot();
+    }
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0f)
+        {
+            Fire();
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+    private void Fire()
+    {
+        GameObject laser = Instantiate(
+            projectile,
+            transform.position,
+            Quaternion.identity
+            ) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+    }
+
+    /// <summary>
+    /// 
+    /// neu health nho hon hoac bang 0 thi huy gameObject
+    /// </summary>
+    /// <param name="lasers"></param>
+    private void OnTriggerEnter2D(Collider2D lasers)
+    {
+        DamageDealer damageDealer = lasers.gameObject.GetComponent<DamageDealer>();
+        if (!damageDealer) { return; }
+        health -= damageDealer.GetDamage();
+        damageDealer.Hit();
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
