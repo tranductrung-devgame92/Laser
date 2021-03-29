836a685700839366629704985547a81e9f8586a0
tranductrung-devgame92 <78129053+tranductrung-devgame92@users.noreply.github.com>
2021-03-29 21:10:32 +0700

    Update DamageDealer.cs

--- Assets/Script/DamageDealer.cs
+++ Assets/Script/DamageDealer.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class DamageDealer : MonoBehaviour
 {
-    //
+    //Set damage ban đầu là 100
     [SerializeField] int damage = 100;
     /// <summary>
-    /// 
+    /// Contructor GetDamage
     /// </summary>
-    /// <returns></returns>
+    /// <returns>giá trị damage</returns>
     public int GetDamage()
     {
         return damage;
     }
 
     /// <summary>
-    /// 
+    /// Contructor Hit dùng để xóa bỏ gameObject
     /// </summary>
     public void Hit()
     {
